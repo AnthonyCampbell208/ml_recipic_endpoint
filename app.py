1194c73343f356de355c48697b36d8bedc643860
AnthonyCampbell208 <78286293+AnthonyCampbell208@users.noreply.github.com>
2022-07-13 16:48:11 -0400

    work

--- app.py
+++ app.py
@@ -23,14 +23,14 @@ def predict():
     # convert data into dataframe
     return jsonify(data)
 
-    # predictions
-    result = model.predict(data_df)
+    # # predictions
+    # result = model.predict(data_df)
 
-    # send back to browser
-    output = {'results': int(result[0])}
+    # # send back to browser
+    # output = {'results': int(result[0])}
 
-    # return data
-    return jsonify(result)
+    # # return data
+    # return jsonify(result)
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
